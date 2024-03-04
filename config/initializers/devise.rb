df081c035e6dde92ce0dcc11a973a77f06770fd7
Kolosek-test <134289262+Kolosek-test@users.noreply.github.com>
2024-03-04 21:32:35 +0100

    Update devise.rb remove o_auth on test env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:oauth_client_id], Rails.application.credentials.google[:oauth_secret]
+  if Rails.env.development? || Rails.env.production?
+    config.omniauth :google_oauth2, Rails.application.credentials.google[:oauth_client_id], Rails.application.credentials.google[:oauth_secret]
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
